ab2bd48f5f79ccfd85b413a022b00a9e8216f1f6
[Vendel17HUN] <[jaromivendel2@gmail.com]>
2020-11-11 15:22:35 +0100

    First 6 was easy.

---
+++ src/com/company/HelloOthers.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import javax.sound.midi.Soundbank;
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Üdvözöld 3 osztálytársad 3 sorban kiiratva a nevüket, ehhez hasonlóan:
+        //
+        // Helló Eszter!
+        // Helló Maris!
+        // Helló Józsi!
+        System.out.println("Helló Eszter!");
+        System.out.println("Helló Maris!");
+        System.out.println("Helló Józsi!");
+    }
+}
